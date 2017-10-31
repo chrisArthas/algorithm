ef0644a3b8f2ec1c9a7e5b8197bcd678df628918
Chris Wu <chris.wsy@hotmail.com>
2017-10-31 17:58:01 +0800

    单列表转换成双链表

--- 二叉搜索树转换成双向链表/FirstRange.java
+++ 二叉搜索树转换成双向链表/FirstRange.java
@@ -11,11 +11,17 @@ public class FirstRange {
 		// TODO Auto-generated method stub
 		TreeNode treeNode = new TreeNode(10,new TreeNode(6,new TreeNode(4,null,null),new TreeNode(8,new TreeNode(7,null,null),new TreeNode(9,new TreeNode(31,null,null),null))),new TreeNode(14,new TreeNode(12,null,null),new TreeNode(16,null,null)));
 		convert(treeNode);
+		convertDouble(treeNode);
 		print(treeNode);
 	
 	}
 	
 	
+	/**
+	 * 先利用递归变成单向链表
+	 * @param currentNode
+	 * @return
+	 */
 	public static TreeNode convert(TreeNode currentNode)
 	{
 		TreeNode lastNode = null;
@@ -50,7 +56,18 @@ public class FirstRange {
 		return lastNode;
 	}
 	
-	
+	/**
+	 * 转换成双向链表
+	 */
+	public static void convertDouble(TreeNode root)
+	{
+		TreeNode node = root;
+		while(node.left != null)
+		{
+			node.left.right = node;
+			node = node.left;
+		}
+	}
 	
 	public static void print(TreeNode treeNode)
 	{
